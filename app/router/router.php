d85b0c8e119b1fc73164728a95ee3108dbf2e8b4
Heitor Zelbato <heitorzelbato@gmail.com>
2025-05-15 17:15:15 -0300

    Alterando nomes das Rotas

--- app/router/router.php
+++ app/router/router.php
@@ -1,7 +1,7 @@
 <?php
 
 
-function routes():array
+function routes(): array
 {
     return require 'routes.php';
 }
@@ -14,15 +14,40 @@ function exactMatchUriInArrayRoutes($uri, $routes)
     return [];
 }
 
-function regularExpressionMatchArrayRoutes($uri, $routes):array{
+function regularExpressionMatchArrayRoutes($uri, $routes): array
+{
     return array_filter(
-            $routes,
-            function ($value) use ($uri) {
-                $regex = str_replace('/', '\/', ltrim($value, '/'));
-                return preg_match("/^$regex/", ltrim($uri, '/'));
-            },
-            ARRAY_FILTER_USE_KEY
+        $routes,
+        function ($value) use ($uri) {
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+            return preg_match("/^$regex/", ltrim($uri, '/'));
+        },
+        ARRAY_FILTER_USE_KEY
+    );
+}
+
+
+function params($uri, $matchedUri)
+{
+    if (!empty($matchedUri)) {
+        $matchToGetParams = array_keys($matchedUri)[0];
+        return array_diff(
+            explode('/', ltrim($uri, '/')),
+            explode('/', ltrim($matchToGetParams, '/'))
         );
+    }
+    return [];
+}
+
+function formatParam($uri, $params)
+{
+    $uri = explode('/', ltrim($uri, '/'));
+    $paramsData = [];
+    foreach ($params as $index => $param) {
+        $paramsData[$uri[$index - 1]] = $param;
+    }
+
+    return $paramsData;
 }
 
 function router()
@@ -32,11 +57,33 @@ function router()
 
     $routes = routes();
 
+    // $arry1 = [
+    //     'user',
+    //     '1',
+    //     'name',
+    //     'Alexandre'
+    // ];
+
+    // $arry2 = [
+    //     'user',
+    //     '[0-9]+',
+    //     'name',
+    //     '[a-z]+'
+    // ];
+
+    // var_dump(array_diff($arry1, $arry2));
+    // die();
+
     $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
     if (empty($matchedUri)) {
         $matchedUri = regularExpressionMatchArrayRoutes($uri, $routes);
+        $params = params($uri, $matchedUri);
+        $params = formatParam($uri, $params);
+
+        // var_dump($paramsData);
+        die();
     }
 
-    var_dump($matchedUri);
-    die();
+    // var_dump($matchedUri);
+    // die();
 }
--- app/router/routes.php
+++ app/router/routes.php
@@ -3,7 +3,8 @@
     return [
         '/' => 'Home@index',
         '/user/create' => 'User@create',
-        '/user/[a-z0-9]+' => 'User@index'
+        '/user/[0-9]+' => 'User@index',
+        '/user/[0-9]+/name/[a-z]+' => 'User@show'
     ];
 
 ?>
